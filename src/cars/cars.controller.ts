b64baecd4aeecf35084d8463f93bd0a4c5c04bcc
Songezo Kenene <songezo.kensonn@gmail.com>
2022-06-26 19:02:55 +0200

    initial commit

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, Req, Request } from '@nestjs/common';
+
+@Controller('cars')
+export class CarsController {
+    @Get()
+    findAll(@Req() request: Request): {} {
+        return [{make: 'honda', model: 'accord'},
+        {make: 'subaru', model: 'outback'},
+        {make: 'fiat', model: '123 spider'}];
+    }
+
+}
